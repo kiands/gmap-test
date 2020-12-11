4229f0fcc1975a292f5e19a4d1222718aa1016d3
Zuofei Han <hzf@ZuofeideMacBook-Pro.local>
2020-12-11 11:21:41 +0800

    completed

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <button @click="minus()">-</button>
-        <input id="value" v-model="info"/>
-        <button @click="plus()">+</button>
+        <button v-bind:disabled="minus_isDisabled" @click="minus()">-</button>
+        <input v-model="info" v-bind:class="{ input1: isDisabled }" />
+        <button v-bind:disabled="plus_isDisabled" @click="plus()">+</button>
     </div>
 </template>
 
@@ -11,16 +11,39 @@ export default {
   name: 'Counter',
   data () {
     return {
-      info: 0
+      info: 0,
+      isDisabled: true,
+      minus_isDisabled: true,
+      plus_isDisabled: false
     }
   },
   methods: {
     minus: function() {
       this.info = this.info - 1
+      if ( this.info == 0) {
+        this.isDisabled = true
+        this.minus_isDisabled = true
+      } else {
+        this.isDisabled = false
+        this.plus_isDisabled = false
+      }
     },
     plus: function() {
       this.info = this.info + 1
+      if ( this.info == 10 ) {
+        this.isDisabled = true
+        this.plus_isDisabled = true
+      } else {
+        this.isDisabled = false
+        this.minus_isDisabled = false
+      }
     }
   }
 }
 </script>
+
+<style>
+  .input1 {
+    background-color: red
+  }
+</style>
