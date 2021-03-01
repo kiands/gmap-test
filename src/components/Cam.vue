fcd5041f460ab65c5184ac6974d50f76ec05b541
kiands <794583439@qq.com>
2021-03-01 13:26:06 +0800

    phone

--- src/components/Cam.vue
+++ src/components/Cam.vue
@@ -80,7 +80,7 @@
         
         // 思考：created和mounted和updated的區別以及何時使用this
         // 获取用户媒体,包含视频和音频
-        navigator.mediaDevices.getUserMedia({video: { facingMode: { exact: "environment" } }, audio: false}).then(stream => {
+        navigator.mediaDevices.getUserMedia({ video: { facingMode: { exact: "environment" } }, audio: false }).then(stream => {
           video.srcObject = stream;// 将捕获的视频流传递给video，放弃window.URL.createObjectURL(stream)的使用
           video.play();// 播放视频
           //audio.srcObject = stream;
@@ -90,8 +90,8 @@
         video.addEventListener('canplay', function(ev){
           if (!this.streaming) {
             //height = video.videoHeight / (video.videoWidth/width);
-            canvas.setAttribute('width', 500);
-            canvas.setAttribute('height', 375);
+            canvas.setAttribute('width', 375);
+            canvas.setAttribute('height', 500);
             this.streaming = !this.streaming;
           }
         }, false)
@@ -103,7 +103,7 @@
         shutter() {
           //需要判断媒体流是否就绪
           if(!this.streaming){
-            canvas.getContext("2d").drawImage(video, 0, 0, 600, 450);// 将视频画面捕捉后绘制到canvas里面
+            canvas.getContext("2d").drawImage(video, 0, 0, 450, 600);// 将视频画面捕捉后绘制到canvas里面
             this.album.push(canvas.toDataURL('image/png'))
             //img.src = canvas.toDataURL('image/png');// 将canvas的数据传送到img里
             //var parent = document.querySelector("#album");;// 添加div
